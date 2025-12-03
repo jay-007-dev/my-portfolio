9e81142144fa4420efe937324698192eb78994fc
jasondeleo1992-dev <jasondeleo1992@gmail.com>
2025-12-03 12:32:02 -0500

    fixed some issue

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -4,7 +4,7 @@ function About() {
           <div className="max-w-7xl mx-auto px-6">
             <h1 className="text-5xl font-semibold mb-6 transition-all duration-700 ease-out">About Me</h1>
             <p className="text-lg text-white/80 leading-relaxed transition-all duration-700 ease-out delay-150">
-              Hi, I’m Jason Deleo, a Senior Full Stack Software Engineer with over 10 years of professional experience building scalable, cloud-native, and high-performance web applications.
+              Hi, I’m Jason Deleo, a Senior Software Engineer with over 10 years of professional experience building scalable, cloud-native, and high-performance web applications.
               I specialize in .NET Core, Python (Django/FastAPI), and React, delivering end-to-end solutions that combine robust backend logic with intuitive, modern front-end experiences. Over the years, I’ve designed and deployed distributed microservices architectures across AWS, Azure, and GCP, optimizing performance, scalability, and cost efficiency.
             </p>
             <div className="flex justify-center mt-8 ">
